2eead0ef09e984a4a567ae42d94b7dd5480b9862
Wade Womersley <wade85@gmail.com>
2015-03-06 15:21:42 +0000

    Added helpers for logging to AMQP/RabbitMQ

---
+++ com/xcitestudios/Logging/AMQPExchangeLogger.cs
@@ -0,0 +1,46 @@
+﻿namespace com.xcitestudios.Logging
+{
+    using com.xcitestudios.Logging.Interfaces;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// Helper for AMPQ message loggers.
+    /// </summary>
+    public abstract class AMQPExchangeLogger
+    {
+        /// <summary>
+        /// Generate an exchange key from the ILogMessage.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns>dot seperated string</returns>
+        protected string GenerateKey(ILogMessage message)
+        {
+            List<string> parts = new List<string>();
+
+            parts.Add(message.Source);
+            parts.Add(message.Application);
+
+            if (message.Module != null)
+            {
+                parts.Add(message.Module);
+            }
+
+            parts.Add(message.Severity.ToString());
+
+            return string.Join(".", parts.ToArray());
+        }
+
+        /// <summary>
+        /// Convert the message to JSON.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        protected string GetMessageJSON(ILogMessage message)
+        {
+            return (message as ILogMessageSerializable).SerializeJSON();
+        }
+    }
+}
---
+++ com/xcitestudios/Logging/RabbitMQMessageLogger.cs
@@ -0,0 +1,49 @@
+﻿namespace com.xcitestudios.Logging
+{
+    using com.xcitestudios.Logging.Interfaces;
+    using RabbitMQ.Client;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class RabbitMQMessageLogger : AMQPExchangeLogger, ILogger
+    {
+        /// <summary>
+        /// Channel to push using.
+        /// </summary>
+        protected IModel Channel { get; set; }
+
+        /// <summary>
+        /// Exchange to push to.
+        /// </summary>
+        protected string ExchangeName { get; set; }
+
+        /// <summary>
+        /// Create a new RabbitMQMessageLogger.
+        /// </summary>
+        /// <param name="channel">Channel connection</param>
+        /// <param name="exchangeName">Exchange to work on</param>
+        public RabbitMQMessageLogger(IModel channel, string exchangeName)
+        {
+            Channel = channel;
+            ExchangeName = exchangeName;
+        }
+
+        /// <summary>
+        /// Log the message to RabbitAMQP
+        /// </summary>
+        /// <param name="message">Message to Log</param>
+        public void Log(Interfaces.ILogMessage message)
+        {
+            var bytes = Encoding.UTF8.GetBytes(this.GetMessageJSON(message));
+
+            var properties = Channel.CreateBasicProperties();
+            properties.Headers = (IDictionary<string, object>)new Dictionary<string, object>();
+            properties.Headers.Add("content-type", "application/json");
+
+            Channel.BasicPublish(ExchangeName, GenerateKey(message), properties, bytes);
+        }
+    }
+}
